0230eed45b921a4d128abdcf11b2f96a976ee268
denisjul <mr.jdenis@gmail.com>
2017-11-07 22:55:12 +0100

    test depuis petit pc

---
+++ democratos/democratos/CreateYourLaws/dl_law_codes/stock_update.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+try:
+    from dl_law_codes.classe_pdf import Table
+    from models import LawArticle
+except:
+    from CreateYourLaws.dl_law_codes.classe_pdf import Table
+    from CreateYourLaws.models import LawArticle
+
+
+def SU_Article(NewArticle, link, container, Law_Code, Gov):
+    """ Ad the new Article in the DB"""
+    NewArticlehtml = ""
+    for x in NewArticle.contents:
+        if isinstance(x, Table):
+            NewArticlehtml += x.html
+        else:
+            NewArticlehtml += x
+    Articlesql, created = LawArticle.objects.get_or_create(
+        autor=Gov,
+        title=NewArticle.name,
+        law_code=Law_Code,
+        block=container)
+    Articlesql.url = link
+    if created:
+        Articlesql.text_law = NewArticlehtml
+        Articlesql.updated = True
+        Articlesql.save()
+    else:
+        if Articlesql.text_law != NewArticlehtml:
+            Articlesql.autor = Gov
+            Articlesql.title = NewArticle.name
+            Articlesql.text_law = NewArticlehtml
+            Articlesql.law_code = Law_Code
+            Articlesql.block = container
+        Articlesql.updated = True
+        Articlesql.save()
+    print(Articlesql.law_code.title,
+          " n°",
+          Articlesql.law_code.id,
+          ': ',
+          Articlesql.title,
+          ' --> OK!')
